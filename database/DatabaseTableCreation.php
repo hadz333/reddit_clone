88011357e0a270ada37f33aaa055f83b20341431
hadz333 <dhadzic@uw.edu>
2020-05-19 16:38:41 -0700

    database created, users table created

---
+++ database/DatabaseTableCreation.php
@@ -0,0 +1,32 @@
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "RedditCloneDB";
+
+// creation of Users table in RedditCloneDB database
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // sql to create table
+    $sql = "CREATE TABLE Users (
+    username VARCHAR(30) PRIMARY KEY,
+    password VARCHAR(40) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )";
+
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "Table created successfully";
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+$conn = null;
+?>
