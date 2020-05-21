957f206ddb5f9cf1467345be6e3cf6843ab27e6c
hadz333 <dhadzic@uw.edu>
2020-05-21 11:39:01 -0700

    home page is showing a list of posts

--- homePage.php
+++ homePage.php
@@ -13,16 +13,65 @@
   ?>
 
 <?php
-
 if (isset($_SESSION["username"])) {
 	echo "<a href='create_post.php'>+ Create post</a>";
 } else {
 	echo "<a href='login_page.php'>+ Create post</a>";
 }
 
+echo "<h2>Posts</h2>";
+
+echo "<table style='border: solid 0px black;'>";
+echo "<tr><th>Author</th><th>Title</th><th>Posted</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current() {
+        return "<td style='width:150px;border:0px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() {
+        echo "<tr>";
+    }
+
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+}
+
+$servername = "localhost";
+$db_user = "root";
+$db_password = "";
+$dbname = "RedditCloneDB";
+
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $db_user, $db_password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // begin the transaction
+    $conn->beginTransaction();
+    
+	$query = "SELECT username, title, reg_date FROM posts";
+	$stmt = $conn->prepare($query);
+	$stmt->execute();
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
+} catch(PDOException $e) {
+    // roll back the transaction if something failed
+    $conn->rollback();
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
 ?>
-
 </div>
-
 </body>
 </html>
