0cdafbdf9248ca17169d90d7457c0d075af23364
hadz333 <dhadzic@uw.edu>
2020-05-21 10:20:43 -0700

    create post page implemented

--- create_post.php
+++ create_post.php
@@ -26,37 +26,17 @@ $validInput = true;
 // checks to make sure server request used POST method
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   // error text only shows up when required field is not entered
-  if (empty($_POST["username"])) {
-  	$usernameErr = "Username is required";
+  if (empty($_POST["title"])) {
+  	$titleErr = "Title is required";
   	$validInput = false;
   } else {
-  	$username = test_input($_POST["username"]);
-  	// if name isn't valid, set error text (not blank anymore)
-	if (!preg_match("/^[a-zA-Z0-9]*$/",$username)) {
-	  $usernameErr = "Only letters and numbers allowed";
-	  $validInput = false;
-	}
-
+  	$title = $_POST["title"];
   }
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
+  if (empty($_POST["body"])) {
+    $bodyErr = "Body text is required";
     $validInput = false;
   } else {
-    $email = test_input($_POST["email"]);
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-  		$emailErr = "Invalid email format";
-  		$validInput = false;
-	}
-  }
-  if (empty($_POST["password"])) {
-    $passwordErr = "Password is required";
-    $validInput = false;
-  } else {
-    $password = test_input($_POST["password"]);
-    if (!validate_password($password)) {
-    	$validInput = false;
-  		$passwordErr = "Password must be 6 characters or greater, include 1 Uppercase letter (A-Z) and 1 Number (0-9)";
-	}
+  	$body = $_POST["body"];
   }
 
   if ($validInput) {
@@ -73,69 +53,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 	    // begin the transaction
 	    $conn->beginTransaction();
-	    // our SQL statements
-	    // ensure username and email don't already exist in database
-	    $nameDBcheck = $conn->prepare("SELECT * FROM users WHERE username=?");
-	    $nameDBcheck->execute([$username]);
-	    $result = $nameDBcheck->fetch();
-	    if ($result) {
-	    	$usernameErr = "Username already exists";
-	    	$validInput = false;
-	    }
-
-	    $emailDBcheck = $conn->prepare("SELECT * FROM users WHERE email=?");
-	    $emailDBcheck->execute([$email]);
-	    $result = $emailDBcheck->fetch();
-	    if ($result) {
-	    	$emailErr = "Email already exists";
-	    	$validInput = false;
-	    } 
+	    
 	    if ($validInput) {
-	    	$query = "INSERT INTO users (username, password, email) 
+	    	$query = "INSERT INTO posts (username, title, body) 
 	    		VALUES (?, ?, ?)";
 	    	$stmt = $conn->prepare($query);
-	    	$stmt->execute([$username, $password, $email]);
+	    	$stmt->execute([$_SESSION["username"], $title, $body]);
 	    	// commit the transaction
 		    $conn->commit();
-		    echo "New user created successfully";
-		    header("Location: http://127.0.0.1/phpProjects/reddit_clone/login_page.php");
+		    echo "New post created successfully";
+		    header("Location: http://127.0.0.1/phpProjects/reddit_clone/homePage.php");
 		}
-
-	    
-	    }
-	catch(PDOException $e)
-	    {
+	} catch(PDOException $e) {
 	    // roll back the transaction if something failed
 	    $conn->rollback();
 	    echo "Error: " . $e->getMessage();
-	    }
-
+	}
 	$conn = null;
   }
-}
-
-function validate_password($str) {
-	if (strlen($str) >= 6) {
-		if(preg_match('/[A-Z]/', $str)){
- 			// There is one uppercase letter
- 			if(preg_match('/[0-9]/', $str)){
- 			// There is one number
- 				return true;
-			}
-		}
-	}
-	return false;
-}
-
-/* 
-What is the htmlspecialchars() function?
-The htmlspecialchars() function converts special characters to HTML entities. This means that it will replace HTML characters like < and > with &lt; and &gt;. This prevents attackers from exploiting the code by injecting HTML or Javascript code (Cross-site Scripting attacks) in forms.
-*/
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
 }
 ?>
 
@@ -144,13 +79,13 @@ function test_input($data) {
 <h4 class="error">Required fields: *</h4> 
 <!-- $_SERVER["PHP_SELF"] returns your current file name --> 
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-Title: <input type="text" name="title" value="<?php echo $title;?>">
+Title: <input type="text" name="title" value="<?php echo $title;?>" maxlength="50">
 <!-- span below will only contain "*" (indicating required field) unless error exists -->
 <span class="error">* <?php echo $titleErr;?></span>
 <br><br>
 Body: <textarea id="body" name="body" rows="5" cols="50"><?php echo $body;?></textarea>
 <span class="error">* <?php echo $bodyErr;?></span>
-<br>
+<br><br>
 <input type="submit" name="submit" value="Add Post">
 </form>
 </div>
